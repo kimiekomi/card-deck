4c90d6676afc27a9e3d352a4d31e28219b3d3be7
kimiekomi <kimiekomi1@gmail.com>
2022-03-26 10:33:31 -0600

    created simply script to display cards

--- card_deck.py
+++ card_deck.py
@@ -3,51 +3,13 @@
 debug = True
 trace = True
 
-class CardDeck():
+class Card():
+    pass
+
+class Deck():
 
     def __init__(self):
-        self.ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-        self.values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-        self.suits = ["spades", "clubs", "hearts", "diamonds"]
-
-        self.standard_deck = []
-
-        suits_index = 0
-
-        for i in range(len(self.ranks)):
-            if trace: print(f"looping through rank: '{self.ranks[i]}' and value: '{self.values[i]}'")
-
-            # self.standard_deck.append([self.ranks[i], self.values[i]])
-
-            for i in range(len(self.suits)):
-                
-                if suits_index <= len(self.suits):
-                    if trace: print(f"looping through suit: '{self.suits[i]}'")
-    
-                    self.standard_deck.insert(2, self.suits[i])
-        
-                    suits_index += 1
-    
-                    if suits_index > 3:
-                        suits_index -= 4
-    
-        print(self.standard_deck)
-
-        # for card in self.standard_deck:
-        #     for i in range(len(self.suits)):
-                
-        #         if suits_index <= len(self.suits):
-        #             if trace: print(f"looping through suit: '{self.suits[i]}'")
-    
-        #             card.insert(2, self.suits[suits_index])
-        
-        #             suits_index += 1
-    
-        #             if suits_index > 3:
-        #                 suits_index -= 4
-
-        # print(self.standard_deck)
-    
+        pass
 
 
     def shuffle(self):
@@ -58,17 +20,25 @@ class CardDeck():
         pass
 
 
-def play_cards():
-    if debug: print("initialized create_deck()")
+def display_cards():
+    if debug: print("initialized display_cards()")
     
-    card_deck = CardDeck()
+    ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    suits = [1, 2, 3, 4]
 
-    # card_deck.shuffle()
+    cards = []
 
-    return card_deck
+    for rank in ranks:
+        if trace: print(f"looping through rank: '{rank}'")
+        
+        for suit in suits:
+            if trace: print(f"looping through suit: '{suit}'")
+            cards.append([rank, suit])
+
+    return cards
     
 
     
 if __name__ == "__main__":
-    play_cards()
+    print(display_cards())
 
