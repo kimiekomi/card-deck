8f5f785a626b8508d176cfc55d3cf9b29d7d37b4
Shelby Kim <93174976+kimiekomi@users.noreply.github.com>
2022-03-27 16:50:28 +0000

    redefined global variables

--- copy_deck.py
+++ copy_deck.py
@@ -5,6 +5,16 @@ import random
 debug = True
 trace = False
 
+Ace = 1
+Jack = 11
+Queen = 12
+King = 13
+
+Spades = 1
+Clubs = 2
+Hearts = 3
+Diamonds = 4
+
 class Card():
 
     def __init__(self, suit, rank, value):
@@ -13,20 +23,72 @@ class Card():
         self.value = value
 
 
-    def compare_suit(self, card1, card2):
-        return card1.self.suit() == card2.self.suit() 
+    def same_suit(self, card):
+        return self.suit == card.suit 
         
 
-    def compare_rank(self, card1, card2):
-        return card1.self.rank() == card2.self.rank() 
+    def compare_rank(self, card):
+        return self.rank - card.rank 
 
 
-    def compare_value(self, card1, card2):
-        return card1.self.value() == card2.self.value() 
+    def compare_value(self, card):
+        return self.value - card.value
+
+
+    def greater_rank(self, card):
+        return self.rank > card.rank
+
+
+    def equal_rank(self, card):
+        return self.rank == card.rank
+
+
+    def lesser_rank(self, card):
+        return self.rank < card.rank
+
+
+    def greater_value(self, card):
+        return self.value > card.value
+
+
+    def equal_value(self, card):
+        return self.value == card.value
+
+
+    def lesser_value(self, card):
+        return self.value < card.value
 
     
     def __repr__(self):
-        return f"'{self.rank}' of '{self.suit}', value: '{self.value}'"
+        if self.suit == Spades:
+            suit = "Spades"
+
+        elif self.suit == Clubs:
+            suit = "Clubs"
+
+        elif self.suit == Hearts:
+            suit = "Hearts"
+
+        elif self.suit == Diamonds:
+            suit = "Diamonds"
+            
+            
+        if self.rank == Ace:
+            rank = "Ace"
+
+        elif self.rank == Jack:
+            rank = "Jack"
+
+        elif self.rank == Queen:
+            rank = "Queen"
+
+        elif self.rank == King:
+            rank = "King" 
+
+        else:
+            rank = str(self.rank)
+        
+        return f"{rank} of {self.suit}, value: {self.value}"
 
 
     def print(self):
@@ -35,30 +97,15 @@ class Card():
 
 class Deck():
 
-    def __init__(self):
-        self.ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
-        self.suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    def __init__(self, ace_value=1):
+
+        self.ranks = [Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King]
+        self.suits = [Spades, Clubs, Hearts, Diamonds]
 
         self.reset()
                 
 
     def reset(self):
-        for rank in self.ranks:
-            if rank == "Ace":
-                value = 1
-    
-            elif rank == "Jack":
-                value = 11
-    
-            elif rank == "Queen":
-                value = 12
-    
-            elif rank == "King":
-                value = 13
-    
-            else:
-                value = rank
-                
         self.deck = []
         
         for suit in self.suits:
@@ -91,32 +138,46 @@ class Deck():
             print(card)
 
 
-class WarGame():
-    pass
-            
-        
 if __name__ == "__main__":
-    # ace_of_spades = Card("Spades", "Ace", 1)
-    # two_of_clubs = Card("Clubs", 2, 2)
-    # jack_of_diamonds = Card("Diamonds", "Jack", 11)
-    # queen_of_hearts = Card("Hearts", "Queen", 12)
-    # king_of_diamonds = Card("Diamonds", "King", 13)
+    ace_of_spades = Card(Spades, Ace, 1)
+    two_of_clubs = Card(Clubs, 2, 2)
+    two_of_spades = Card(Spades, 2, 2)
+    jack_of_diamonds = Card(Diamonds, Jack, 11)
+    queen_of_hearts = Card(Hearts, Queen, 12)
+    king_of_diamonds = Card(Diamonds, King, 13)
 
-    # ace_of_spades.print()
-    # two_of_clubs.print()
-    # jack_of_diamonds.print()
-    # queen_of_hearts.print()
-    # print(king_of_diamonds)
+    ace_of_spades.print()
+    two_of_clubs.print()
+    jack_of_diamonds.print()
+    queen_of_hearts.print()
+    print(king_of_diamonds)
 
+    # print(ace_of_spades.same_suit(two_of_spades))
+    # print(ace_of_spades.same_suit(two_of_clubs))
     
-    deck = Deck()
+    # print(two_of_clubs.compare_rank(two_of_spades))
+    # print(two_of_clubs.compare_rank(ace_of_spades))
+    
+    # print(two_of_clubs.compare_value(two_of_spades))
+    # print(jack_of_diamonds.compare_value(two_of_spades))
+
+    # print(jack_of_diamonds.greater_rank(two_of_spades))
+    # print(queen_of_hearts.equal_rank(two_of_spades))
+    # print(ace_of_spades.lesser_rank(two_of_spades))
+
+    # print(jack_of_diamonds.greater_rank(two_of_spades))
+    # print(queen_of_hearts.equal_rank(two_of_spades))
+    # print(ace_of_spades.lesser_rank(two_of_spades))
+
+    # deck = Deck(ace_value=14)
     # deck.print ()
 
-    deck.shuffle()
+    # deck.shuffle()
     # deck.print()
 
-    deck.reset()
+    # deck.reset()
     # deck.print()
 
-    print (deck.get_card())
-
+    # print(deck.get_card())
+    # deck.print()
+        
---
+++ war_game.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from copy_deck import *
+
+debug = True
+trace = False
+
+class WarGame():
+    def __init__(self):
+        # create deck
+        # initialize player1, player2 score...which will be num of cards in hand - 26
+        # deal cards to each player
+
+
+if __name__ == "__main__":
+    pass
