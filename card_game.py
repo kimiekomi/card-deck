ec1b71702c5c405f1ac440ce47015bb758b799cc
Shelby Kim <93174976+kimiekomi@users.noreply.github.com>
2022-03-30 17:16:10 +0000

    created Player, Human, and Computer classes

--- card_game.py
+++ card_game.py
@@ -6,10 +6,32 @@ from deck_class import *
 class CardGame:
 
     def __init__(self):
-
         self.deck = Deck(self, shuffled=True)
 
 
+class Player:
+    def __init__(self, hand):
+        self.hand = hand
+        self.purse = ""
+        self.wager = ""
+
+
+    def hit(self):
+        pass
+
+
+class Human(Player):
+    def __init__(self, hand):
+        super().__init__(hand)
+
+
+
+class Computer(Player):
+    def __init__(self, hand):
+        super().__init__(hand)
+        
+
+    
 if __name__ == "__main__":
     game = CardGame()
 
