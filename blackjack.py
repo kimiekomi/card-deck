5b53b6423836af0abde06b5413334918189ed8e7
Shelby Kim <93174976+kimiekomi@users.noreply.github.com>
2022-03-30 01:46:11 +0000

    created blackjack.py

---
+++ blackjack.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+from card_class import *
+from deck_class import *
+from card_game import CardGame
+
+debug = False
+trace = False
+
+class BlackJack(CardGame):
+
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    blackjack = BlackJack()
+
